f603743e5a8996a331c6fbcba2c7091da320ed9f
JeJo <email2jejo@gmail.com>
2018-11-30 14:48:09 +0100

    new algo for checking dups in or not in a container

--- Template__Extra_Algorithms/main_test.cc
+++ Template__Extra_Algorithms/main_test.cc
@@ -4,14 +4,25 @@
 
 // own header
 #include "Template_count_adjacent_if.h"
+#include "Template_duplicates.h"
 
 int main()
 {
-#if 0 // Test 1
+#if 0 // Test 1: count_adjacent_if
 	std::vector<int> vec{ 1,1,2,4,5,5,6,6 };
 	auto BinPred = [](const auto& lhs, const auto& rhs) { return lhs + rhs == 3 ; };
 	std::cout << ::count_adjacent_if(vec.begin(), vec.end()) <<'\n';
-	std::cout << ::count_adjacent_if2(vec.cbegin(), vec.cend(), BinPred) <<'\n';
+	std::cout << ::count_adjacent_if2(vec.cbegin(), vec.cend(), BinPred) <<'\n'; // error
+#endif
+#if 1 // Test 2: has_duplicates
+	std::vector<char> vec0 = { 'c','a','i','o','p','a' };
+	std::cout << std::boolalpha << ::has_duplicates(vec0.cbegin(), vec0.cend()) << '\n';
+
+	std::vector<int> vec1 = { 1, 2, 8, 1, 2, 7, 2 };
+	std::cout << std::boolalpha << ::has_duplicates(vec1.cbegin(), vec1.cend()) << '\n';
+
+	std::vector<std::string> vec2 = { "Je", "Jo", "Ji", "Jy", "Ja", "Jc", "Jq" };
+	std::cout << std::boolalpha << ::has_duplicates(vec2.cbegin(), vec2.cend()) << '\n';
 #endif
 	return 0;
 }
