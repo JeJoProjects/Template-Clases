2da974bfb7c7e989bfe1eed88aa144b4dd89fa37
JeJo <email2jejo@gmail.com>
2019-01-10 14:23:16 +0100

    fraction

--- Fractions/TemplateClass_Fractions.h
+++ Fractions/TemplateClass_Fractions.h
@@ -1,89 +1,72 @@
-
-#include <iostream>
 #include <algorithm>
+#include <cmath>
 
-class Rational
+class Rational final
 {
 private:
-    int m_numerator, m_denominator;
+	int m_numerator, m_denominator;
 private:
-    inline void simplificate()
-    {
-        int commondivisor = 1;
-        for(int i=2;i<= std::min(abs(m_numerator), abs(m_denominator));i++)
-            if( m_numerator%i == 0 && m_denominator%i == 0 )
-                commondivisor = i;
-        m_numerator /= commondivisor;
-        m_denominator /= commondivisor;
-    }
-    /*inline void swap(Rational& other)
-    {
-        std::swap(m_numerator, other.m_denominator);
-        std::swap(m_denominator, other.m_denominator);
-    }*/
+	inline void simplificate()
+	{
+		int commondivisor = 1;
+		for (int i = 2; i <= std::min(std::abs(m_numerator), std::abs(m_denominator)); i++)
+			if (m_numerator%i == 0 && m_denominator%i == 0)
+				commondivisor = i;
+		m_numerator /= commondivisor;
+		m_denominator /= commondivisor;
+	}
+	/*inline void swap(Rational& other)
+	{
+		std::swap(m_numerator, other.m_denominator);
+		std::swap(m_denominator, other.m_denominator);
+	}*/
 public:
-    
-    Rational()                                  // Defualt
-        :m_numerator(1), m_denominator(1)
-        {}
+	Rational() = default;
+	Rational(const int num, const int den = 1)
+		:m_numerator(num), m_denominator(den)
+	{}
 
-    Rational(const int& num, const int& den=1)  // Parameterized
-        :m_numerator(num), m_denominator(den)
-        {}
+	// Copy
+	Rational(const Rational& other) = default;
+	Rational& operator= (Rational const& other) = default;
 
-    Rational(const Rational& other)             // Copy
-        :m_numerator(other.m_numerator), m_denominator(other.m_denominator)
-        {}
+	Rational& operator/ (const int divisor) 
+	{
+		m_denominator *= divisor;
+		simplificate();
+		return *this;
+	}
 
-    Rational(Rational&& other)                  // Move
-        :m_numerator(other.m_numerator), m_denominator(other.m_denominator)
-        {}
-    /*Rational(Rational&& other)                // another Move
-        :m_numerator(0), m_denominator(0)
-        {   swap(other);    }*/
+	Rational& operator/= (const Rational &divisor)
+	{
+		m_numerator *= divisor.m_denominator;
+		m_denominator *= divisor.m_numerator;
+		simplificate();
+		return *this;
+	}
 
-    ~Rational(){}
+	const double getrealformat()const
+	{
+		return  static_cast<double>(m_numerator) /
+			static_cast<double>(m_denominator);
+	}
 
-    Rational& operator= (Rational const& other) = default;
-    Rational& operator/ (const int& divisor)
-    {
-            m_denominator *= divisor;
-            simplificate();
-            return *this;
-    }
-    Rational& operator/= (const Rational &divisor)
-    {
-        m_numerator *= divisor.m_denominator;
-        m_denominator *= divisor.m_numerator;
-        simplificate();
-        return *this;
-    }
-    const double getrealformat()const
-    {
-        return  static_cast<double>(m_numerator)/
-                static_cast<double>(m_denominator);
-    }
-
-    friend double operator/ (Rational& obj, const int& divisor);
-    friend Rational operator/ (Rational A, Rational const & B);
-    friend void printRational(Rational& obj, const int& A, const int& B);
+	friend Rational operator/ (Rational A, Rational const & B);
+	friend void printRational(Rational& obj, const int& A, const int& B);
 };
+
 /************************** Friend functions ********************************/
 Rational operator/ (Rational A, Rational const& B)
 {
-    return std::move(A/=B);
+	return std::move(A /= B);
 }
-double operator/ (Rational& obj, const int& divisor)
-{
-    obj.m_denominator *= divisor;
-    obj.simplificate();
-    return obj.getrealformat();
-}
+
 void printRational(Rational& obj, const int& A, const int& B)   // lvalue
 {
-    Rational r1(A), r2(B);
-    obj = r1/r2;
-    std::cout<<obj.m_numerator<<'/'<<obj.m_denominator<<std::endl;
-    //std::cout<<obj.getrealformat()<<std::endl;
+	Rational r1(A), r2(B);
+	obj = r1 / r2;
+	std::cout << obj.m_numerator << '/' << obj.m_denominator << std::endl;
+	std::cout<<obj.getrealformat()<<std::endl;
 }
+
 /*****************************************************************************/
