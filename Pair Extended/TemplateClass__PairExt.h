33f7e3e42876be0753e136ef9f133f6717c47849
JeJo <email2jejo@gmail.com>
2019-01-11 16:27:09 +0100

    pair class

--- Pair Extended/TemplateClass__PairExt.h
+++ Pair Extended/TemplateClass__PairExt.h
@@ -1,29 +1,31 @@
 /* 
  */
 
-#include <iostream>
+#include <iostream>    
 
-template<typename T, typename U>
-struct Pair;
+template<typename T, typename U> struct Pair;
 
 template<typename T, typename U>
 std::ostream& operator<<(std::ostream & out, const Pair<T, U>  &p);
 
 template<typename T, typename U>
-struct Pair : public std::pair<T, U>
+struct Pair final : public std::pair<T, U>
 {
 	using std::pair<T, U>::pair;
-	Pair& operator++()
+
+	Pair& operator++() noexcept
 	{
 		this->first++; this->second++;
 		return *this;
 	}
-	Pair operator++(int) // post-increment, return unmodified copy by value
+
+	Pair operator++(int) noexcept // post-increment, return unmodified copy by value
 	{
 		Pair copy(*this);
 		++(*this); // or operator++();
 		return copy;
 	}
+
 	friend std::ostream& operator<< <>(std::ostream & out, const Pair  &p);
 };
 
