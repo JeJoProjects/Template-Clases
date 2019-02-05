eb0236a065f3cfe0477100013c27678cefa78d0f
JeJo Home <email2jejo@gmail.com>
2019-02-05 23:22:29 +0100

    new test cases for FractionT

--- Fractions/main_test.cc
+++ Fractions/main_test.cc
@@ -9,10 +9,13 @@
 // test code
 int main()
 {
-	Fraction<int> obj( 6, 8 );
-	std::cout << obj << " " << obj.getReal() << std::endl;
+	for (int i = 0; i <= 80; ++i)
+	{
+		Fraction<int> obj(i, 8);
+		std::cout << obj << " " << obj.getReal() << std::endl;
+	}
 
-	// @todo: implimentation for float and doubles
+	// @todo: implementation for float and doubles
 	// Fraction<float> obj1(3.5f, 1.0f);
 	// std::cout << obj1 << " " << obj1.getReal() << std::endl;
 	return 0;
