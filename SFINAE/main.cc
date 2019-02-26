37e0a14c32d84df124ec6619140d0cdd96a984f6
Somewhere on Earth <email2jejo@gmail.com>
2019-02-26 12:21:46 +0100

    SFINAE: : change of C++ headers .h -> .hpp

---
+++ SFINAE/main.cc
@@ -0,0 +1,15 @@
+// C++ headers
+#include <iostream>
+
+// own header
+#include "SFINAE.hpp"
+
+int main()
+{
+	std::cout << Function(nullptr)	<< " ";
+	std::cout << Function(2)		<< " ";
+	std::cout << Function(2.0f)		<< " ";
+	std::cout << Function((2.0))	<< "\n";
+
+	return  0;
+}
