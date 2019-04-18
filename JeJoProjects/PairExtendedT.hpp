adeba2f8e471bfe5cc1de74b18925773bfdc5103
Somewhere on Earth <email2jejo@gmail.com>
2019-04-18 14:44:04 +0200

    PairExtendedT new codes

--- JeJoProjects/PairExtendedT.hpp
+++ JeJoProjects/PairExtendedT.hpp
@@ -1,31 +1,75 @@
-/*
- */
+/******************************************************************************
 
+******************************************************************************/
+
+#ifndef JEJO_PAIR_EXTENDED_T_HPP
+#define JEJO_PAIR_EXTENDED_T_HPP
+
+ // macros for name-spacing
+#define JEJO_BEGIN namespace JeJo {
+#define JEJO_END }
+
+// C++ headers
 #include <iostream>
 
+JEJO_BEGIN
+
+// forward declaration(s)
 template<typename T, typename U> struct PairExt;
-
 template<typename T, typename U>
-std::ostream& operator<<(std::ostream & out, const PairExt<T, U>  &p) noexcept;
+std::ostream& operator<<(std::ostream &out, const PairExt<T, U>  &p) noexcept;
 
+// PairExt<> definition
 template<typename T, typename U>
-struct PairExt final : public std::pair<T, U>
+struct PairExt final
 {
-	using std::pair<T, U>::pair;
+	T first{};
+	U second{};
 
+	PairExt(const T &fir, const U &sec) noexcept : first{fir}, second{sec} {}
+
+	// Copy : enabled
+	PairExt(const PairExt &other) noexcept
+		: first{ other.first },
+		second{ other.second }
+	{}
+	PairExt& operator=(const PairExt &other) noexcept
+	{
+		this->first = other.first;
+		this->second = other.second;
+		return *this;
+	}
+
+	// Move : enabled
+	explicit constexpr PairExt(PairExt &&other) noexcept
+		: first{std::move(other.first)},
+		second{std::move(other.second)}
+	{}
+
+	PairExt& operator=(PairExt &&other) noexcept
+	{
+		this->first = std::move(other.first);
+		this->second = std::move(other.second);
+		return *this;
+	}
+
+	// overloaded operator(s)  of the class
+	// @todo: should be enabled only for integral types!
 	PairExt& operator++() noexcept
 	{
 		this->first++; this->second++;
 		return *this;
 	}
 
-	PairExt operator++(int) noexcept // post-increment, return unmodified copy by value
+	// post-increment, return unmodified copy by value
+	PairExt operator++(int) noexcept
 	{
-		PairExt copy(*this);
+		PairExt copy{ *this };
 		++(*this); // or operator++();
 		return copy;
 	}
 
+	// non-member function(s)
 	friend std::ostream& operator<< <>(
 		std::ostream & out, const PairExt  &p) noexcept;
 };
@@ -36,3 +80,15 @@ std::ostream& operator<<(std::ostream & out, const PairExt<T, U>  &p) noexcept
 	return out << p.first << " " << p.second << std::endl;
 }
 
+// make_pair() to construct a PairExt<> : similar to std::make_pair
+template<typename T, typename U>
+PairExt<T, U> make_pair(T first, U second) noexcept
+{
+	return PairExt<T, U>{first, second};
+}
+
+JEJO_END
+
+#endif // JEJO_PAIR_EXTENDED_T_HPP
+
+/*****************************************************************************/
